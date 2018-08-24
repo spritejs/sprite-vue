6bd6cae61456fb370b2473916635af7a60980dcb
akira-cn <akira.cn@gmail.com>
2018-08-24 22:37:06 +0800

    attribute bug fixed

--- src/components/Sprite.vue
+++ src/components/Sprite.vue
@@ -1,7 +1,7 @@
 <template>
   <scene id="container" :resolution="[1200, 1200]" :viewport="[600, 600]">
     <layer id="fglayer">
-      <sprite :pos="[100, 100]" :bgcolor="bgcolor" :size="[100, 100]"></sprite>
+      <sprite :pos="[100, 100]" :bgcolor="bgcolor" :size="[100, 100]" v-on:click="onClick"></sprite>
       <label :pos="[300, 100]" :width="300" font="24px '宋体'" lineBreak="normal">
         Hello Sprite,Hello Sprite,Hello Sprite,Hello Sprite,Hello Sprite,Hello Sprite
         Hello Sprite,Hello Sprite,Hello Sprite,Hello Sprite,Hello Sprite,Hello Sprite
@@ -18,6 +18,11 @@ export default {
       bgcolor: 'green',
     };
     return window.test;
+  },
+  methods: {
+    onClick(evt) {
+      console.log(evt);
+    },
   },
 };
 </script>
