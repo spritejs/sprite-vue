80771cf898a3913281e1d06d39188430855171b7
akira-cn <akira.cn@gmail.com>
2018-09-07 10:19:18 +0800

    :bug: fixed animation bug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-import Vue from 'web/entry-runtime-with-compiler'
+import BaseVue from 'web/entry-runtime-with-compiler'
 import * as spritejs from 'spritejs'
+
+function compileProto (proto) {
+  const { states, actions } = proto
+  const exData = {}
+  if (states) exData.states = states
+  if (actions) exData.actions = actions
+
+  if (typeof proto.data === 'function') {
+    const _getter = proto.data
+    proto.data = function (...args) {
+      const data = Object.assign(exData, _getter.call(this, ...args))
+      return data
+    }
+  } else {
+    Object.assign(proto.data, exData)
+  }
+
+  delete proto.states
+  delete proto.actions
+
+  return proto
+}
+
+class Vue extends BaseVue {
+  static component (name, proto) {
+    console.log(proto)
+    return super.component(name, compileProto(proto))
+  }
+  constructor (proto) {
+    super(compileProto(proto))
+  }
+}
 
 export {
   Vue,
