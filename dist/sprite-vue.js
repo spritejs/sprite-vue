18b3e69cf00b9ff3e611a1930a51c58957fb5f91
akira-cn <akira.cn@gmail.com>
2018-11-26 18:38:48 +0800

    :bug: fixed viewport bug

--- dist/sprite-vue.js
+++ dist/sprite-vue.js
@@ -9839,7 +9839,7 @@ function Paper2D() {
   return new (Function.prototype.bind.apply(_scene2.default, [null].concat(args)))();
 }
 
-var version = '2.22.13';
+var version = '2.22.14';
 
 exports._debugger = _platform._debugger;
 exports.version = version;
@@ -34839,7 +34839,9 @@ var ExLayer = function (_Layer) {
               _width = _canvas$getBoundingCl.width,
               _height = _canvas$getBoundingCl.height;
 
-          return [_width, _height];
+          if (_width > 0) {
+            return [_width, _height];
+          }
         }
         if (canvas.clientWidth) return [canvas.clientWidth, canvas.clientHeight];
       }
