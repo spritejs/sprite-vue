55d28c3f83822f780ddc5f55f3d2c6fedcc030b8
akira-cn <akira.cn@gmail.com>
2019-05-15 16:33:23 +0800

    :bug:fixed nested group cache release bug

--- dist/sprite-vue.js
+++ dist/sprite-vue.js
@@ -9909,7 +9909,7 @@ function Paper2D() {
   return _babel_runtime_helpers_construct__WEBPACK_IMPORTED_MODULE_0___default()(Scene, args);
 }
 
-var version = "2.27.15";
+var version = "2.27.16";
 
 
 /***/ }),
@@ -15514,7 +15514,7 @@ var BaseSprite = _babel_runtime_helpers_decorate__WEBPACK_IMPORTED_MODULE_6___de
           }
         }
 
-        if (this.cacheContext && context !== this.cacheContext) {
+        if (this.cacheContext && context !== this.cacheContext && !this.cacheContext.__lockTag) {
           _utils__WEBPACK_IMPORTED_MODULE_11__["cacheContextPool"].put(this.cacheContext);
         }
 
@@ -15787,6 +15787,8 @@ var BaseSprite = _babel_runtime_helpers_decorate__WEBPACK_IMPORTED_MODULE_6___de
         if (cachableContext) {
           // set cache before render for group
           if (!this.cache) {
+            cachableContext.__lockTag = true; // cannot put back to Pool while drawing.
+
             this.cache = cachableContext;
             this.render(t, cachableContext);
           }
@@ -15824,6 +15826,9 @@ var BaseSprite = _babel_runtime_helpers_decorate__WEBPACK_IMPORTED_MODULE_6___de
         this.dispatchEvent('afterdraw', evtArgs, true, true);
 
         if (cachableContext) {
+          delete cachableContext.__lockTag; // release lockTag
+
+          if (!this.cache) _utils__WEBPACK_IMPORTED_MODULE_11__["cacheContextPool"].put(cachableContext);
           cachableContext.restore();
         }
 
