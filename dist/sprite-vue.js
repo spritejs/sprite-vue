ca16eb7bf67f19c091069929e5a5771f9fae226c
akira-cn <akira.cn@gmail.com>
2019-05-22 10:16:01 +0800

    :bug:fixed cache conflict bug

--- dist/sprite-vue.js
+++ dist/sprite-vue.js
@@ -9909,7 +9909,7 @@ function Paper2D() {
   return _babel_runtime_helpers_construct__WEBPACK_IMPORTED_MODULE_0___default()(Scene, args);
 }
 
-var version = "2.27.17";
+var version = "2.27.18";
 
 
 /***/ }),
@@ -15831,7 +15831,6 @@ var BaseSprite = _babel_runtime_helpers_decorate__WEBPACK_IMPORTED_MODULE_6___de
         this.dispatchEvent('afterdraw', evtArgs, true, true);
 
         if (cachableContext) {
-          if (!this.cache) _utils__WEBPACK_IMPORTED_MODULE_11__["cacheContextPool"].put(cachableContext);
           cachableContext.restore();
         }
 
